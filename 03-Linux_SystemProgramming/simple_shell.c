a17e2edd873b78eb6a4d082d511f7531e17a60f9
osama <usamamahmoud.2022@gmail.com>
2025-04-06 09:31:11 +0200

    implement simple shell

---
+++ 03-Linux_SystemProgramming/simple_shell.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define BUF_SIZE  100
+int main()
+{
+
+    char buf[BUF_SIZE];
+
+    while (1) {
+	printf("I am a simple shell, enter your command $ ");
+	fgets(buf, BUF_SIZE, stdin);
+	buf[strlen(buf) - 1] = 0;
+	if (strlen(buf) == 0)
+	    continue;
+	pid_t pid = fork();
+	if (pid > 0) {
+	    int status;
+	    printf("Parent: my pid = %d, my child pid = %d\n", getpid(),
+		   pid);
+	    wait(&status);
+	    printf("Parent: my pid = %d, my child status = %d\n", getpid(),
+		   WEXITSTATUS(status));
+	} else if (pid == 0) {
+	    printf("Child: my pid = %d, my parent pid = %d\n", getpid(),
+		   getppid());
+
+	    char *newargv[] = { buf, NULL };
+	    char *newenvp[] = { NULL };
+	    execve(buf, newargv, newenvp);
+	    printf
+		("Exec failed, kernel is not the mode of executing programs\n");
+	    exit(-1);
+
+	} else {
+	    printf("Parent: failed to fork\n");
+	}
+
+    }
+
+    return 0;
+
+}
